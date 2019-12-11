af2c9073af989db9e605277a18647d923c3c4731
liyinggit <2501830002@qq.com>
2019-12-11 11:21:06 +0800

    阿里移动推送配置

--- android/app/src/main/kotlin/com/example/flutter_community/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_community/MainActivity.kt
@@ -6,8 +6,30 @@ import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+  private val TAG = "Init"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+    initCloudChannel(this);
   }
+
+  /**
+     * 初始化云推送通道
+     * @param applicationContext
+     */
+    private void initCloudChannel(Context applicationContext) {
+        PushServiceFactory.init(applicationContext);
+        CloudPushService pushService = PushServiceFactory.getCloudPushService();
+        pushService.register(applicationContext, new CommonCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Log.d(TAG, "init cloudchannel success");
+            }
+            @Override
+            public void onFailed(String errorCode, String errorMessage) {
+                Log.d(TAG, "init cloudchannel failed -- errorcode:" + errorCode + " -- errorMessage:" + errorMessage);
+            }
+        });
+    }
 }
